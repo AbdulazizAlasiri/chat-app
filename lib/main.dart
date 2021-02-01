043955913e3a7b8083a5faee82f66070a475d500
Abdulaziz Alasiri <Abdulaziz.A.Alasiri@gmail.com>
2021-02-01 20:52:33 +0300

    Setting up environment

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,7 +19,13 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        'chat': (context) => ChatScreen(),
+      },
     );
   }
 }
